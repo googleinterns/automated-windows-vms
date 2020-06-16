75ba64aeeb359050193dbacabc472c8ee5a7164e
mayank-ch <66247814+mayank-ch@users.noreply.github.com>
2020-06-17 04:02:19 +0530

    Add files via upload

---
+++ master_server/dummy_vm_server.py
@@ -0,0 +1,104 @@
+"""
+  This is a dummy VM server.
+  It is built to test master server.
+"""
+import sys
+import os
+import threading
+from datetime import datetime
+import requests
+from flask import Flask
+from flask import request
+
+
+class MyFlaskApp(Flask):
+  """
+    This class configures the flask app and executes function
+    pre_task before starting flask server.
+  """
+  def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
+    if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
+      with self.app_context():
+        pre_task()
+    super(MyFlaskApp, self).run(
+        host=host, port=int(sys.argv[1]), debug=debug,
+        load_dotenv=load_dotenv, **options)
+
+app = MyFlaskApp(__name__)
+
+flag = False
+master_server = 'http://127.0.0.1:5000'
+vm_address = 'http://127.0.0.1:'
+request_id = ''
+
+def task_done():
+  """
+    It prints time and then sleep for some time
+    and then again print time.
+  """
+  global flag
+  now = datetime.now()
+  current_time = now.strftime('%H:%M:%S')
+  print('Current Time =', current_time)
+  os.system('sleep 10')
+  now = datetime.now()
+  current_time = now.strftime('%H:%M:%S')
+  print('Current Time =', current_time)
+  task_completed()
+  flag = False
+  register_vm_address()
+
+def task_completed():
+  """Notify the master server after task is completed.
+  """
+  req = requests.get(master_server+str('/success'), data=request_id)
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+  """
+    This function receives files from master server.
+    And executes some task to make the VM busy.
+  """
+  global flag
+  global request_id
+  input_file = request.files['task_request']
+  req = request.files['request_id'].read()
+  req = req.decode('utf-8')
+  request_id = req
+  flag = True
+  t = threading.Thread(target=task_done)
+  t.start()
+  return 'success'
+
+@app.route('/active', methods=['GET', 'POST'])
+def f():
+  """Master can check here if VM is active or not.
+  """
+  return {'hello': 'world'}
+
+@app.route('/status', methods=['GET', 'POST'])
+def g():
+  """returns the state of VM
+  """
+  return {'status': flag}
+
+def register_vm_address():
+  """This functions tells the master server that VM is healhty.
+  """
+  data = 'http://127.0.0.1:'+str(sys.argv[1])
+  try:
+    req = requests.get(master_server+str('/register'), data=data)
+  except:
+    print('can''t connect to master server')
+
+def pre_task():
+  """This function performs tasks before the start of flask server.
+  """
+  if len(sys.argv) != 2:
+    print("Usage:", sys.argv[0], "INPUT_PORT")
+    sys.exit(-1)
+  vm_address = 'http://127.0.0.1:' + sys.argv[1]
+  register_vm_address()
+
+if __name__ == '__main__':
+  app.run()
---
+++ master_server/master_server.py
@@ -0,0 +1,221 @@
+#!/usr/bin/python
+
+"""This script works on the asssumption of a certain directory structure.
+
+
+     It then acts the master server and load balances the incoming
+     requests.
+     A database is created to store the incoming requests from user.
+"""
+import os
+import requests
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+import Request_pb2
+
+class MyFlaskApp(Flask):
+  """
+    This class configures the flask app and executes function
+    pre_task before starting flask server.
+  """
+  def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
+    if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
+      with self.app_context():
+        pre_tasks()
+    super(MyFlaskApp, self).run(
+        host=host, port=port, debug=debug, load_dotenv=load_dotenv, **options)
+
+app = MyFlaskApp(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+working_vm_address = []
+available_vm_address = []
+
+class Newrequestentry(db.Model):
+  """
+    This class creates a table Newrequestentry
+    init function in this class inserts a new row
+    the table.
+  """
+  request_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  proto_file = db.Column(db.BLOB)
+  request_status = db.Column(db.String(50))
+  last_vm_assigned = db.Column(db.String(100))
+  number_of_retries_allowed = db.Column(db.Integer)
+  number_of_retries_till_now = db.Column(db.Integer)
+
+  def __init__(
+      self, proto_file, request_status, last_vm_assigned,
+      number_of_retries_allowed, number_of_retries_till_now):
+    self.proto_file = proto_file
+    self.request_status = request_status
+    self.last_vm_assigned = last_vm_assigned
+    self.number_of_retries_allowed = number_of_retries_allowed
+    self.number_of_retries_till_now = number_of_retries_till_now
+
+def create_new_request(
+    proto_file, request_status, last_vm_assigned,
+    number_of_retries_allowed, number_of_retries_till_now):
+  """This function adds a new row to database.
+  """
+  dessert = Newrequestentry(
+      proto_file, request_status, last_vm_assigned, number_of_retries_allowed,
+      number_of_retries_till_now)
+  db.session.add(dessert)
+  db.session.commit()
+  return dessert
+
+def get_status(request_id):
+  """Get status of previous request.
+  """
+  get_row_data = Newrequestentry.query.get(request_id)
+  return get_row_data
+
+def change_state(request_id):
+  """Change request_status of a request.
+  """
+  Newrequestentry.query.filter_by(request_id=request_id).\
+  update({Newrequestentry.request_status: 'completed'})
+  db.session.commit()
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+  print(' You have reached master server \n')
+
+@app.route('/register', methods=['GET', 'POST'])
+def add_vm_address():
+  """Add a VM to working_vm_address list.
+  """
+  address = request.data.decode('utf-8')
+  if address not in working_vm_address:
+    working_vm_address.append(address)
+  return {'message': 'success'}
+
+@app.route('/upload')
+def upload_file():
+  """Serve web page
+  """
+  return render_template('upload.html')
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_fil():
+  """
+    Accept proto file from user and
+    assign it to VM.
+  """
+  f = request.files['file']
+  read = f.read()
+  vm_n = find_working_vm()
+  print(vm_n)
+  if vm_n == 'NOT':
+    return 'try again later'
+  else:
+    try:
+      working_vm_address.remove(vm_n)
+      task_request = Request_pb2.TaskRequest()
+      task_request.ParseFromString(read)
+      requ = create_new_request(
+          read, 'accepted', vm_n, task_request. numner_of_retries, 0)
+      response = requests.post(url=vm_n, files={'task_request': read,\
+      'request_id': ('', str(requ.request_id))})
+      return 'file uploaded successfully,your request_id is '+ \
+          str(requ.request_id)+'  '+' address is '+str(requ.last_vm_assigned)+ \
+          ' number of retries allowed is '+str(requ.number_of_retries_allowed)
+    except:
+      return 'try again later'
+
+@app.route('/vm_status', methods=['GET', 'POST'])
+def upload_fi():
+  """Return VM address which are active.
+  """
+  return str(working_vm_address)
+
+@app.route('/success', methods=['GET', 'POST'])
+def task_completed():
+  """Completed request
+  """
+  request_id = request.data.decode('utf-8')
+  change_state(int(request_id))
+  return 'success'
+
+@app.route('/request_status', methods=['GET', 'POST'])
+def status_of_request():
+  """Check status of a request
+  """
+  req_id = request.form['request_id']
+  request_row = get_status(req_id)
+  if request_row is None:
+    return 'invalid request'
+  else:
+    return request_row.request_status
+
+def is_healthy(node):
+  """
+    Check given VM is healthy or not.
+    Args:
+      node:
+  """
+  try:
+    req = requests.get(node+str('/active'))
+    return True
+  except:
+    return False
+
+def is_engaged(node):
+  """
+    Check whether given VM is engaged in any
+    task or not.
+    Args:
+      node:
+  """
+  if is_healthy(node):
+    try:
+      req = requests.get(node+str('/status'))
+      a = req.json()
+      return bool(a['status'])
+    except:
+      return False
+  return False
+
+def find_working_vm():
+  """
+    Find working VM address.
+  """
+  for i in working_vm_address:
+    if not is_engaged(i):
+      return i
+  return 'NOT'
+
+def get_working_vm_address():
+  """
+    Append IP addresses of working VMs to a list.
+  """
+  for i in available_vm_address:
+    if is_healthy(i) and not is_engaged(i):
+      working_vm_address.append(i)
+
+def get_available_vm_address():
+  """
+    Append IP addresses of available VMs to a list.
+    It assumes IP addresses are present in a text file.
+  """
+  with open('listfile.txt', 'r') as filehandle:
+    for line in filehandle:
+      new_address = line[:-1]
+      available_vm_address.append(new_address)
+
+def pre_tasks():
+  """
+    Tasks to do before flask server starts.
+  """
+  db.create_all()
+  get_available_vm_address()
+  get_working_vm_address()
+
+if __name__ == '__main__':
+  app.run()
