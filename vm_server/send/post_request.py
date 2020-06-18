800cadd3a364fc32fea942a3f2e32121d033bd7b
NKH123 <neeraj.deshpande3@gmail.com>
2020-06-19 03:24:44 +0530

    Add logging in the response file and fix comment spacing

--- vm_server/send/post_request.py
+++ vm_server/send/post_request.py
@@ -7,16 +7,17 @@
   sends it to the VM server, and accepts the response as
   response.pb and saves it in .\\proto\\ directory
 """
+import logging
 import os
 import requests
 
-
+logging.basicConfig(filename='response.log', level=logging.DEBUG)
 URL = "http://127.0.0.1:5000/load"
 ROOT = ".\\proto\\"
 with open(ROOT + 'input_request.pb', 'rb') as input_request:
   RESPONSE = requests.post(url=URL, files={'task_request': input_request})
-print(type(RESPONSE))
-print(str(RESPONSE.content))
+logging.debug(type(RESPONSE))
+logging.debug(str(RESPONSE.content))
 if os.path.exists(ROOT + "response.pb"):
   os.remove(ROOT + "response.pb")
 with open(ROOT + "response.pb", "wb") as f:
