a69ac814721f90c9dcb6f056c00ffe964240d184
NKH123 <neeraj.deshpande3@gmail.com>
2020-07-02 06:14:07 +0530

    Copies to the specified output directory now

--- vm_server/accept/server.py
+++ vm_server/accept/server.py
@@ -19,7 +19,7 @@ from vm_server.send.proto import Request_pb2
 
 sem = threading.Semaphore()
 
-def remove_execute_dir():
+def remove_execute_dir(task_response):
   """Deletes the execute directory if it exists
   
   Args:
@@ -45,12 +45,13 @@ def make_directories(task_request, task_response):
     task_response: an object of TaskResponse() that will be sent back
   """
   logging.debug("Creating execute directory structure")
-  remove_execute_dir()
+  remove_execute_dir(task_response)
   if task_response.status == Request_pb2.TaskResponse.FAILURE:
     return
   current_path = "..\\execute\\action"
   os.mkdir("..\\execute")
   os.mkdir(current_path)
+  os.mkdir(current_path + "\\output\\")
   Path("..\\execute\\__init__.py").touch() # __init__.py for package
   Path(current_path + "\\__init__.py").touch()
   try: 
@@ -62,6 +63,27 @@ def make_directories(task_request, task_response):
     logging.exception(str(exception))
     print("Error copying code and data directories")
     logging.debug("Error copying code and data directories")
+    task_response.status = Request_pb2.TaskResponse.FAILURE
+
+def move_output(task_request, task_response):
+  """Move the genrated output files to the output path specified
+
+  Args:
+    task_request: an object of TaskResponse() that is sent in the request
+    task_response: an object of TaskResponse() that will be sent back
+  """
+  logging.debug("Moving the output path to the specified output path")
+  source_path = "..\\execute\\action\\output\\"
+  destination_path = task_request.output_path
+  files = os.listdir(source_path)
+  try:
+    for file in files:
+      shutil.move(source_path + file, destination_path)
+  except Exception as exception:  #catch errors if any
+    print(exception)
+    logging.exception(str(exception))
+    print("Error moving the output files to the specified output directory")
+    logging.debug("Error moving the output files to the specified output directory")
     task_response.status = Request_pb2.TaskResponse.FAILURE
 
 
@@ -82,12 +104,12 @@ def execute_action(task_request, task_response):
   out = None
   err = None
   try:
-    execute = subprocess.Popen(["powershell.exe", #  execute the target file
+    execute = subprocess.Popen(["powershell.exe", # execute the target file
                                 task_request.target_path],
                                stdout=subprocess.PIPE,
                                cwd=current_path)
     out, err = execute.communicate(timeout=task_request.timeout)
-  except Exception as exception:  #catch errors if any
+  except Exception as exception:  # catch errors if any
     print(exception)
     print("FAIL")
     logging.debug(str(exception))
@@ -105,9 +127,14 @@ def execute_action(task_request, task_response):
     std_err = open(current_path + "\\output\\stderr.txt", "w")
     std_err.write(err.decode(encoding))
     std_err.close()
+    output_files = [name for name in os.listdir("..\\execute\\action\\output\\")
+                    if os.path.isfile("..\\execute\\action\\output\\" + name)]
+    task_response.number_of_files = len(output_files)
+    move_output(task_request, task_response)
   except Exception as exception:
     print("Error writing in std out, stderr", exception)
     logging.debug("Error writing in std out, stderr" + str(exception))
+    task_response.status = Request_pb2.TaskResponse.FAILURE
 
 
 APP = Flask(__name__)
@@ -130,9 +157,6 @@ def load():
     print("Time taken is ", time_taken)
     logging.debug("Time taken is " + str(time_taken))
     task_response.time_taken = time_taken
-    output_files = [name for name in os.listdir("..\\execute\\action\\output\\")
-                    if os.path.isfile("..\\execute\\action\\output\\" + name)]
-    task_response.number_of_files = len(output_files)
     if task_response.status != Request_pb2.TaskResponse.FAILURE:
       task_response.status = Request_pb2.TaskResponse.SUCCESS
     current_path = os.path.dirname(os.path.realpath("__file__"))
