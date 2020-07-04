7abc4bc0e8f1b9f8aec6015fff4f15047aa65c9b
mayank-ch <66247814+mayank-ch@users.noreply.github.com>
2020-07-04 19:58:39 +0530

    Update start.py

--- master_server/start.py
+++ master_server/start.py
@@ -10,8 +10,8 @@ def new_dummy_server():
 def master_server():
   os.system('python master_server.py')
 if __name__ == '__main__':
-  t = threading.Thread(target = master_server)
-  t.start()
+#  t = threading.Thread(target = master_server)
+#  t.start()
   count = int(sys.argv[2])
   for i in range(count):
     t = threading.Thread(target = new_dummy_server)
