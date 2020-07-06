280bee5a5f30fe4f679e22f547a3eb931214799c
mayank-ch <mayankch@google.com>
2020-07-06 21:58:22 +0530

    change test.py to include flags.

--- master_server/test.py
+++ master_server/test.py
@@ -1,10 +1,21 @@
+import argparse
 import os
-import threading
 import sys
+import threading
 import time
 import requests
+from google.protobuf import text_format
+import Request_pb2
 
-port = int(sys.argv[1])
+parser = argparse.ArgumentParser()
+
+#-db DATABSE -u USERNAME -p PASSWORD -size 20
+parser.add_argument('-st', '--start_port', help='Starting port number', type=int)
+parser.add_argument('-c', '--count', help='Number of port needed' ,type=int)
+parser.add_argument('-f', '--filename', help='File name of text file')
+parser.add_argument('-n', '--number', help='Number of times to repeat the request', type=int)
+args = parser.parse_args()
+port = args.start_port
 
 def new_dummy_server():
   global port
@@ -15,23 +26,26 @@ def master_server():
   os.system('python master_server.py')
 
 if __name__ == '__main__':
-  if len(sys.argv) != 5:
-    print('Usage:', sys.argv[0], 'INPUT_PORT_START_NUMBER NUMBER_OF_VMs NUMBER_OF_TIMES_TO_SEND_REQUEST_FILE1 NUMBER_OF_TIMES_TO_SEND_REQUEST_FILE2')
-    sys.exit(-1)
+  print( 'Starting_port {} Count {} filename {} number {} '.format(
+        args.start_port,
+        args.count,
+        args.filename,
+        args.number
+        ))
   t = threading.Thread(target = master_server)
   t.start()
   time.sleep(5)
-  count = int(sys.argv[2])
+  count = args.count
   for i in range(count):
     t = threading.Thread(target = new_dummy_server)
     t.start()
     
   time.sleep(5)
-  for i in range(int(sys.argv[3])):
-    with open("input_request_1.pb", "rb") as input_request:
-      RESPONSE = requests.post(url="http://127.0.0.1:5000/assign_task", files={"file": input_request})
-      print(RESPONSE.content)
-  for i in range(int(sys.argv[4])):
-    with open("input_request.pb", "rb") as input_request:
-      RESPONSE = requests.post(url="http://127.0.0.1:5000/assign_task", files={"file": input_request})
-      print(RESPONSE.content)
+  TEXT_FILE = open(args.filename, 'r')
+  TASK_REQUEST = Request_pb2.TaskRequest()
+  text_format.Parse(TEXT_FILE.read(), TASK_REQUEST)
+  TEXT_FILE.close()
+  
+  for i in range(args.number):
+    RESPONSE = requests.post(url='http://127.0.0.1:5000/assign_task', files={'file': TASK_REQUEST.SerializeToString()})
+    print(RESPONSE.content)
