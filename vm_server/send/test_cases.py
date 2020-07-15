cbcad00c750183b468eb815e3f8a6df92e3b3846
NKH123 <neeraj.deshpande3@gmail.com>
2020-07-15 23:00:44 +0530

    Test by comparing with expected output

---
+++ vm_server/send/test_cases.py
@@ -0,0 +1,33 @@
+import test_server
+import filecmp
+
+
+def test_execute_macro():
+  """Test the execute macro action"""
+  execute = test_server.execute_commands(1)
+  output_path = "..\\test\\execute_macro\\output\\output.txt"
+  expected_output_path = "..\\test\\execute_macro\\expected_output\\expected_output.txt"
+  assert filecmp.cmp(output_path, expected_output_path)
+
+def test_screenshot():
+  """Test the word screenshot action"""
+  execute = test_server.execute_commands(2)
+  assert execute == True
+
+def test_timeout():
+  """Test the failure due to timeout action"""
+  execute = test_server.execute_commands(3)
+  assert execute == False
+
+def test_config_pair():
+  """Test if config pairs are being set"""
+  execute = test_server.execute_commands(4)
+  assert execute == True
+
+def test_time_out_multiple_times():
+  """Test failure due to time out multiple times""" 
+  for i in range(3):
+    execute = test_server.execute_commands(3)
+    assert execute == False
+
+
--- vm_server/test/execute_macro/code/execute.py
+++ vm_server/test/execute_macro/code/execute.py
@@ -19,13 +19,15 @@ def execute_macro():
     xl_file = win32com.client.Dispatch("Excel.Application")
     xl_run = xl_file.Workbooks.Open(os.path.abspath(path_to_file),
                                     ReadOnly=1)
-    xl_run.Application.Run("excelsheet.xlsm!main.simpleMain") #execute macro
+    xl_run.Application.Run("excelsheet.xlsm!Module1.add_numbers_in_column") #execute macro
     xl_run.Save()
     xl_run.Close()
     xl_file.Quit()
     del xl_file
     shutil.move(path_to_file, current_path +
                 "\\action\\output\\excelsheet.xlsm")
+    shutil.move(current_path + "\\action\\data\\output.txt", current_path +
+                "\\action\\output\\output.txt")
     print("Action successfully executed")
 
 if __name__ == "__main__":
