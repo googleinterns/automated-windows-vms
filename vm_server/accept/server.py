586722aac9d9b0288d58f52916f68cb36eefb6e9
NKH123 <neeraj.deshpande3@gmail.com>
2020-07-17 19:28:55 +0530

    Add debug statement

--- vm_server/accept/server.py
+++ vm_server/accept/server.py
@@ -308,7 +308,9 @@ def set_environment_variables(task_request):
   Args:
     task_request: an object of TaskResponse() that is sent in the request
   """
+  PRINT("i AM HERE")
   for config_pair in task_request.config_pairs:
+    print("Setting %s to %s", config_pair.key, config_pair.value)
     os.environ[config_pair.key] = config_pair.value
 
 def execute_wrapper(task_request, task_response):
